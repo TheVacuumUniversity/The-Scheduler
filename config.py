129458ef0684d176dc62173defc5ccd37516a056
PavelFryblik <pavel.fryblik@gmail.com>
2019-01-23 11:00:33 +0100

    Delete config,py
    Config should not be stored in public repository.

--- config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    DATABASE_URI = r'postgresql://scheduler:ZbEOEYC2hjABvWGsaNEZ@160.100.70.244:5432/scheduler'
-    #DATABASE_URI = r'postgresql://postgres:postgres@localhost:5432/scheduler'
